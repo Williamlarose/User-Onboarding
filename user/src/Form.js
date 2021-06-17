95b7124483430545cefba7db1d0859372f4dacdd
Williamlarose <38011333+Williamlarose@users.noreply.github.com>
2021-06-17 00:31:31 -0400

    done

---
+++ user/src/Form.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export default function Userform(props) {
+    const {
+        values,
+        submit,
+        change,
+        disabled,
+        errors,
+    } = props
+    function onSubmit(event) {
+        event.preventDefault()
+        submit()
+    }
+    <div>
+        <div>{errors.username}</div>
+        <div>{errors.email}</div>
+        <div>{errors.PasswordTermOfService}</div>
+        <div>{errors.TermOfService}</div>
+    </div>
+
+    function onChange(event) {
+        const { name, value, checked, type } = event.target
+        const valueToUse = type === 'checkbox' ? checked : value
+        change(name, valueToUse)
+    }
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                type='text'
+                name='Name'
+                onChange={onChange}
+                value={values.Name}
+            />
+            <input
+                type='text'
+                name='Email'
+                onChange={onChange}
+                value={values.Email}
+            />
+            <input
+                type='text'
+                name='Password'
+                onChange={onChange}
+                value={values.Password}
+            />
+
+            <div>
+                <label>TermOfService
+                    <input
+                        type='checkbox'
+                        name='Terms'
+                        checked={values.Terms}
+                        onChange={onChange} /> </label> </div>
+            <button disabled={disabled}>submit</button>
+        </form >
+    )
+}
